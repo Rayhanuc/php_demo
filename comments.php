f7b091b779867382a2e795ffe13594588d683355
Rayhanuc <rayhanctg4@gmail.com>
2023-08-15 23:57:35 +0600

    Comments in PHP

---
+++ comments.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Comments</title>
+</head>
+<body>
+  
+  <?php
+  // This function below displays text (Single linke comment)
+
+
+  /* Multiline comment
+  there are some text
+  Another line of text here
+  */
+  echo "Hello Students";
+  
+  ?>
+
+
+</body>
+</html>
