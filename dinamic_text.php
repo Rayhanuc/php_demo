3bda680f9b14b50ea2453d007313e281343fcc26
Rayhanuc <rayhanctg4@gmail.com>
2023-08-15 23:21:06 +0600

    PHP Demo practice

---
+++ dinamic_text.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<?php
+  $title = "Md. Rayhan Uddin Chowdhury Official Site";
+?>
+  
+
+<h1><?php echo $title; ?></h1>
+
+
+</body>
+</html>
