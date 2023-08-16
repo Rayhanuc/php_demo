9aa4a3c549474fad0e1de93d5c2bb6755377dec1
Rayhanuc <rayhanctg4@gmail.com>
2023-08-17 00:31:53 +0600

    PHP Math

---
+++ math.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php
+
+echo 25 + 89;
+echo "<br>";
+echo 52 - 41;
+echo "<br>";
+echo 91 * 25;
+echo "<br>";
+echo 120 / 19;
+echo "<br>";
+
+
+echo 53 + 27 * 15 / 4215 - 8;
+echo "<br>";
+echo (5 + 5) * 10;
+echo "<br>";
+echo (5 + 12) / 14;
+echo "<br>";
+$number1 = 24;
+$number2 = 32;
+echo $number1 * $number2;
+echo "<br>";
+
+
+?>
+</body>
+</html>
