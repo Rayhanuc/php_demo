b73ba94bb5fca82fb2d3d64fcfe50fcf2214273c
Rayhanuc <rayhanctg4@gmail.com>
2023-08-18 02:26:10 +0600

    PHP If statement

---
+++ ifstatement.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP If Statement</title>
+</head>
+<body>
+
+<?php
+
+if (3 > 10) {
+    echo "<h3>Three is less than ten</h3>";
+} elseif(4 > 5) {
+    echo "<h3>Of course four is less than five</h3>";
+} else {
+    echo "<h3>It is not</h3>";
+}
+
+?>
+</body>
+</html>
