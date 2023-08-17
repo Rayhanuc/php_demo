a8cf34bd78f96090e856322642ae99099a730e65
Rayhanuc <rayhanctg4@gmail.com>
2023-08-17 19:04:45 +0600

    Title change

--- associative_arrays.php
+++ associative_arrays.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP Math</title>
+  <title>PHP Associative Arrays</title>
 </head>
 <body>
     <h1>PHP Associative Arrays</h1>
