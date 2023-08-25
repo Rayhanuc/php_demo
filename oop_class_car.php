a9f93b80fc90b72ca0d65fb454cee6a5e5f800ae
Rayhanuc <rayhanctg4@gmail.com>
2023-08-26 03:56:13 +0600

    PHP OOP Class Car

---
+++ oop_class_car.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP OOP Class Car</title>
+</head>
+<body>
+<?php
+class Car {
+
+}
+
+if (class_exists("Car")) {
+    echo "YEayyyyyyyyyyy Nice";
+} else {
+    echo "Noooooo";
+}
+?>
+</body>
+</html>
